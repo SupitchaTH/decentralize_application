6d85add9120a43c9791e9fabbe927406873a5a74
Supitcha Theppithak <supitcha.theppithak@gmail.com>
2024-01-19 14:00:49 +0700

    [Edit] Change value to const variable.

--- lib/app/Widgets/appbar.dart
+++ lib/app/Widgets/appbar.dart
@@ -1,14 +1,18 @@
+import 'package:decentralize_application/app/Utilities/colors.dart';
+import 'package:decentralize_application/app/Utilities/fontSizes.dart';
+import 'package:decentralize_application/app/Utilities/spaces.dart';
 import 'package:flutter/material.dart';
 
 class GlobalAppbar extends StatelessWidget implements PreferredSizeWidget {
   final String appbarText;
 
   const GlobalAppbar({
+    super.key,
     required this.appbarText,
   });
 
   @override
-  Size get preferredSize => const Size.fromHeight(200.0);
+  Size get preferredSize => const Size.fromHeight(spaceXXL);
 
   @override
   Widget build(BuildContext context) {
@@ -17,12 +21,14 @@ class GlobalAppbar extends StatelessWidget implements PreferredSizeWidget {
       child: AppBar(
         title: Text(
           appbarText,
-          textAlign: TextAlign.center,
         ),
+        elevation: 0,
+        backgroundColor: darkOrangeColor,
+        titleTextStyle: welcomeText,
         centerTitle: true,
         shape: const RoundedRectangleBorder(
           borderRadius: BorderRadius.vertical(
-            bottom: Radius.circular(40),
+            bottom: Radius.circular(radiusL),
           ),
         ),
         automaticallyImplyLeading: false,
